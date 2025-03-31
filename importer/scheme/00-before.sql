088c781a7a58a1313e4119dadad7f4656cbec3c1
Guiming <guiming.zhang007@gmail.com>
2025-03-31 09:36:34 -0600

    Do not drop existing tables to allow incremental imports.

--- importer/scheme/00-before.sql
+++ importer/scheme/00-before.sql
@@ -1,6 +1,6 @@
 -- requires hstore_new, postgis, gist_btree
 
-DROP TABLE IF EXISTS hist_point CASCADE;
+--DROP TABLE IF EXISTS hist_point CASCADE;
 CREATE TABLE IF NOT EXISTS hist_point (
     id bigint,
     version smallint,
@@ -14,7 +14,7 @@ CREATE TABLE IF NOT EXISTS hist_point (
     geom geometry(Point, 3857)
 );
 
-DROP TABLE IF EXISTS hist_line CASCADE;
+--DROP TABLE IF EXISTS hist_line CASCADE;
 CREATE TABLE IF NOT EXISTS hist_line (
     id bigint,
     version smallint,
@@ -30,7 +30,7 @@ CREATE TABLE IF NOT EXISTS hist_line (
     geom geometry(LineString, 3857)
 );
 
-DROP TABLE IF EXISTS hist_polygon CASCADE;
+--DROP TABLE IF EXISTS hist_polygon CASCADE;
 CREATE TABLE IF NOT EXISTS hist_polygon (
     id bigint,
     version smallint,
@@ -49,7 +49,7 @@ CREATE TABLE IF NOT EXISTS hist_polygon (
 );
 
 /* GZ */
-DROP TABLE IF EXISTS hist_relation CASCADE;
+--DROP TABLE IF EXISTS hist_relation CASCADE;
 CREATE TABLE IF NOT EXISTS hist_relation (
     id bigint,
     version smallint,
@@ -62,7 +62,7 @@ CREATE TABLE IF NOT EXISTS hist_relation (
     tags hstore
 );
 
-DROP TABLE IF EXISTS hist_relation_member CASCADE;
+--DROP TABLE IF EXISTS hist_relation_member CASCADE;
 CREATE TABLE IF NOT EXISTS hist_relation_member (
     relation_id bigint,
     version smallint,
@@ -72,7 +72,7 @@ CREATE TABLE IF NOT EXISTS hist_relation_member (
     --PRIMARY KEY (relation_id, member_id, version) -- there might be duplicated (relation_id, member_id, version)
 );
 
-DROP TABLE IF EXISTS hist_user CASCADE;
+--DROP TABLE IF EXISTS hist_user CASCADE;
 CREATE TABLE IF NOT EXISTS hist_user (
     user_id bigint PRIMARY KEY,
     user_name text
